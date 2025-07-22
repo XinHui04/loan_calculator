3a94fdd52e305a5c19d972da2064b38e2d07defe
XinHui04 <lxinhui0401@gmail.com>
2025-07-22 15:02:20 +0800

    item added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart' as intl;
 
 void main() {
@@ -47,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _loanPeriod = 1;
   bool _hasGuarantor = false;
   int _carType = 1; // 1 = new 2 = used
-  double _repaymentAMount = 0.0;
+  double _repaymentAmount = 0.0;
   String _repaymentOutput = '';
   final _years = [1,2,3,4,5,6,7,8,9];
 
@@ -75,38 +76,164 @@ class _MyHomePageState extends State<MyHomePage> {
 
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Loan Amount'
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  controller: loanAmountCtrl,
+                  focusNode: _myFocusNode,
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter loan amount';
+                    }return null;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Net Income'
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  controller: netIncomeCtrl,
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter net income';
+                    }return null;
+                  },
+                ),
+                DropdownButtonFormField(
+                  value: _loanPeriod,
+                  items: _years.map((int item){
+                    return DropdownMenuItem(
+                      value: item,
+                      child: Text('$item year(s)' )
+                    );
+                }).toList(),
+                  onChanged: (int? item){
+                    setState(() {
+                      _loanPeriod = item!;
+                    });
+                  }, decoration: const InputDecoration(
+                    labelText: 'Select Loan period (year)'
+                ),
+                  validator: (value){
+                    if(value == 0){
+                      return 'Please select an option';
+                    }return null;
+                  },
+                ),
+                TextFormField(
+                  controller: interestRateCtrl,
+                  keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true,
+                    signed: false,
+                  ),
+                  inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0 - 9]'))],
+                  decoration: const InputDecoration(
+                    labelText: 'Interest rate (%)'
+                  ),
+                  validator: (value){
+                    if(value == null || value.isEmpty){
+                      return 'Please enter interest rate';
+                    }return null;
+                  },
+                ),
+                CheckboxListTile(
+                    value: _hasGuarantor,
+                    title: const Text('I have guarantor.'),
+                    onChanged: (value){
+                      setState(() {
+                        _hasGuarantor = value!;
+                      });
+                    }),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Car Type',
+                    textDirection: TextDirection.ltr,),
+                ),
+                RadioListTile(
+                    value: 1,
+                    title: const Text('New'),
+                    groupValue: _carType,
+                    onChanged: (value){
+                      setState(() {
+                        _carType = value!;
+                      });
+                    }),
+                RadioListTile(
+                    value: 2,
+                    title: const Text('Used'),
+                    groupValue: _carType,
+                    onChanged: (value){
+                      setState(() {
+                        _carType = value!;
+                      });
+                    }),
+                Text(_repaymentOutput),
+                ElevatedButton(
+                    onPressed: (){
+                      if(_formKey.currentState!.validate(){
+                        if(validInterest(_carType)){
+                          _calculateRepayment();
+                        }else{
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content:Text('Invalid Interest rate'))
+                          );
+                      }
+                      }},child: Text('Calcuulator'))
+
+              ],
             ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  void myAlertDialog(){
+    AlertDialog eligibilityAlertDialog = AlertDialog(
+      title: const Text('Eligibility'),
+      content: const Text('You are not eligible for this loan. '
+          'Get a guarantor to proceed'),
+      actions: [
+        TextButton(
+            onPressed: (){
+              Navigator.pop(context);
+            },
+            child: Text('OK'))
+      ],
+    );
+    showDialog(context: context, builder: (BuildContext context){
+      return eligibilityAlertDialog;
+    });
+  }
+  void _calculateRepayment(){
+    _loanAmount = double.tryParse(loanAmountCtrl.text)!;
+    _netIncome = double.tryParse(netIncomeCtrl.text)!;
+    _interestRate = double.tryParse(interestRateCtrl.text)!;
+
+    var interest = _loanAmount * (_interestRate/100) * _loanPeriod;
+    _repaymentAmount = (_loanAmount + interest) / (_loanPeriod * 12);
+
+    bool eligible = _netIncome * 0.3 >= _repaymentAmount;
+    if (eligible || _hasGuarantor){
+      setState(() {
+        _repaymentOutput = 'Repayment Amount : '
+            '${myCurrency.currencySymbol}'
+            '${myCurrency.format(_repaymentAmount)}'
+            '\n'
+            'Eligibility : ${eligible? 'Eligible': 'Not Eligible'}';
+      });
+    }
+  }
 }
+
